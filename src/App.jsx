16988fd8b3e4aa5ad40385032ca8302e84c15600
Tritintruong114 <truongtritin.bee@gmail.com>
2023-06-13 18:08:34 +0700

    This is first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <main className="w-screen">
+      {/* Header */}
+      <Header />
+      {/* Board */}
+    </main>
+  );
+}
+
+export default App;
