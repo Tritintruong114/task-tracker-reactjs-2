903ddfe31b35f7e08194fdf204e2f9f5e8b51961
Tritintruong114 <truongtritin.bee@gmail.com>
2023-06-14 17:59:41 +0700

    This is for UI based

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 const Main = () => {
   return (
     <div className="col-span-4 bg-gray-300/30 p-3 no-scrollbar h-full overflow-scroll">
-      Main
+      <div className="bg-red-300/10 h-full">ABC</div>
     </div>
   );
 };
